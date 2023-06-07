681b68e256552f983d51a2417da09f9521f6b2ad
Lihini <lihiniabeykoon1234@gmail.com>
2023-06-07 14:23:25 +0530

    Commit with update 3

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ console.log("Hello from Lihini! It's a new addition");
 console.log("Hello from Lihini! This for test");
 console.log("With a new update");
 console.log("Update 2");
+console.log("Update 3");
+console.log("Update 4");
